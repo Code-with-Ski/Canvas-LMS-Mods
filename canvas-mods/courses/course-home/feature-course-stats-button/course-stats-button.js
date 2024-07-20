ba93ecee5edbac43ae9b118be24e8d5a4631c4d2
James Sekcienski <james.sekcienski@gmail.com>
2024-07-20 19:22:35 -0400

    Updated the course statistics link so that it doesn't open in a new tab by default

--- canvas-mods/courses/course-home/feature-course-stats-button/course-stats-button.js
+++ canvas-mods/courses/course-home/feature-course-stats-button/course-stats-button.js
@@ -15,7 +15,10 @@
   }
 
   function watchForRightSideButtons() {
-    SkiMonitorChanges.watchForElementByQuery("#right-side div.course-options", addCourseStatisticsButton);
+    SkiMonitorChanges.watchForElementByQuery(
+      "#right-side div.course-options",
+      addCourseStatisticsButton
+    );
   }
 
   function addCourseStatisticsButton(rightSideButtons) {
@@ -30,8 +33,8 @@
     const courseStatisticsButton = document.createElement("a");
     courseStatisticsButton.classList.add("btn", "button-sidebar-wide");
     courseStatisticsButton.href = `${BASE_URL}/courses/${courseId}/statistics`;
-    courseStatisticsButton.target = "_blank";
-    courseStatisticsButton.title = "View course statistics and Code with Ski custom course reports";
+    courseStatisticsButton.title =
+      "View course statistics and Code with Ski custom course reports";
     courseStatisticsButton.innerHTML = `<i class="icon-line icon-analytics"></i> Course Statistics`;
 
     return courseStatisticsButton;

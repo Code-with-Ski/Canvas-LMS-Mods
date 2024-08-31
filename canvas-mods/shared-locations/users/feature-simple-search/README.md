a9b23d634873dad554e93e02e1c867d6f67405b0
James Sekcienski <james.sekcienski@gmail.com>
2024-08-30 22:39:47 -0400

    Added support for Simple Search feature on the user profile showing list of course enrollments.

---
+++ canvas-mods/shared-locations/users/feature-simple-search/README.md
@@ -0,0 +1,3 @@
+# Simple Search
+
+This adds a button in the course enrollments to a simple search modal. It will check for course content based on the given search criteria and the currently shown enrollments. Use the search filters can help narrow the courses to search.
---
+++ canvas-mods/shared-locations/users/feature-simple-search/user-simple-search.js
@@ -0,0 +1,80 @@
+(() => {
+  if (
+    /^\/accounts\/[0-9]+\/users\/[0-9]+/.test(window.location.pathname) ||
+    /^\/accounts\/self\/users\/[0-9]+/.test(window.location.pathname) ||
+    /^\/users\/[0-9]+/.test(window.location.pathname)
+  ) {
+    chrome.storage.sync.get(
+      {
+        userCourseEnrollmentsSimpleSearch: true,
+      },
+      function (items) {
+        if (items.userCourseEnrollmentsSimpleSearch) {
+          addSimpleSearch();
+        }
+      }
+    );
+  }
+
+  function addSimpleSearch() {
+    const coursesHeader = document.querySelector("#courses_list h3");
+    console.log(coursesHeader);
+    if (!coursesHeader) {
+      return;
+    }
+
+    coursesHeader.insertAdjacentHTML(
+      "beforebegin",
+      `<button class="ski-simple-search Button" type="button" tabindex="0" title="Open Simple Search" style="margin-bottom: 0.5rem">
+        <i class="icon-solid icon-search"></i>&nbsp;Simple Search
+      </button>`
+    );
+
+    const dialog = addSimpleSearchModal();
+
+    const simpleSearchButton = document.querySelector(
+      "button.ski-simple-search"
+    );
+    simpleSearchButton?.addEventListener("click", () => {
+      dialog?.showModal();
+    });
+  }
+
+  function addSimpleSearchModal() {
+    const dialog = document.createElement("dialog");
+    dialog.id = "ski-simple-search-dialog";
+    dialog.style.width = "800px";
+    dialog.style.maxWidth = "90%";
+    dialog.style.height = "600px";
+    dialog.style.maxHeight = "90%";
+    dialog.style.resize = "both";
+    dialog.style.overflow = "hidden";
+
+    SkiReport.contextDetails.set("reportContext", "user");
+    const report = new SkiReportCourseSimpleSearch();
+    const reportContainer = report.getReportContainer();
+    reportContainer.style.maxHeight = "calc(100% - 4rem)";
+    reportContainer.style.overflow = "auto";
+    dialog.appendChild(reportContainer);
+
+    const closeButton = document.createElement("button");
+    closeButton.innerText = "Close";
+    closeButton.classList.add("Button", "Button--secondary");
+    closeButton.style.float = "right";
+    closeButton.style.marginTop = "1rem";
+    closeButton.addEventListener("click", () => {
+      closeSimpleSearchModal(dialog);
+    });
+    dialog.appendChild(closeButton);
+
+    const content = document.getElementById("content");
+    content.appendChild(dialog);
+
+    return dialog;
+  }
+
+  function closeSimpleSearchModal(dialog) {
+    // reset?
+    dialog.close();
+  }
+})();
